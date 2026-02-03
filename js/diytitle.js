3dcff2375d219a7920d903524a47ab4ec8a42c3d
zzjhjj <3226187650@qq.com>
2026-02-03 03:51:14 +0000

    updated by github action

---
+++ js/diytitle.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="小塞之家~等你回来~",clearTimeout(titleTime)):(document.title="欢迎回来~"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
